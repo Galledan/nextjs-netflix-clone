46989c911a89e7f47c796e78246302677c9f4a6c
Galledan <onrclklr@gmail.com>
2024-03-20 18:19:11 +0300

    Removed protected routes due to bug in vercel

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,23 +8,6 @@ import useMovieList from "@/hooks/useMovieList";
 import { NextPageContext } from "next";
 import { getSession} from "next-auth/react";
 
-export async function getServerSideProps(context: NextPageContext) {
-  const session = await getSession(context);
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/auth',
-        permanent: false,
-      }
-    }
-  }
-
-  return {
-    props: {}
-  }
-
-}
 
 export default function Home() {
 
