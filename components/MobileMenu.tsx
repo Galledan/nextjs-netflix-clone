07b9b855e0737cd9b521e319f43c21f98ae7d30f
Galledan <onrclklr@gmail.com>
2024-03-21 01:34:56 +0300

    Fixed Mobile Menu

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -13,8 +13,23 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ visible }) => {
     return (
         <div className='bg-black w-56 absolute top-8 left-0 py-5 flex-col border-2 border-gray-900'>
             <div className='flex flex-col gap-4'>
-                <div className='px-3 text-center text-white hover:underline'>
+                <div className="px-3 text-center text-white hover:underline">
                     Home
+                </div>
+                <div className="px-3 text-center text-white hover:underline">
+                    Series
+                </div>
+                <div className="px-3 text-center text-white hover:underline">
+                    Films
+                </div>
+                <div className="px-3 text-center text-white hover:underline">
+                    New & Popular
+                </div>
+                <div className="px-3 text-center text-white hover:underline">
+                    My List
+                </div>
+                <div className="px-3 text-center text-white hover:underline">
+                    Browse by Languages
                 </div>
             </div>
         </div>
