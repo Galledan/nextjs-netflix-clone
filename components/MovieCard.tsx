d566c62d94ffe617e2b7d251195ee296fb7856fb
Galledan <onrclklr@gmail.com>
2024-03-20 16:24:52 +0300

    Added Favorites and My List functionality

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BsFillPlayFill } from 'react-icons/bs';
+import FavoriteButton from './FavoriteButton';
 
 interface MovieCardProps {
     data: Record<string, any>;
@@ -64,7 +65,10 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
           ">
                     <div className="flex flex-row items-center gap-3">
                         <div onClick={() => { }} className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300">
-                            <BsFillPlayFill className="text-black w-4 lg:w-6" />
+                            <BsFillPlayFill size={30} className="text-black w-4 lg:w-6" />
+                        </div>
+                        <div>
+                            <FavoriteButton movieId={data?.id}/>
                         </div>
                     </div>
                     <p className='text-green-400 font-semibold mt-4'>
