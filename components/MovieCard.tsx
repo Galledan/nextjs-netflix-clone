142bc972c4377d6d50a8def73cd583165702bf8f
Galledan <onrclklr@gmail.com>
2024-03-20 17:07:54 +0300

    Added InfoModal

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import { BsFillPlayFill } from 'react-icons/bs';
+import React, { useCallback } from 'react'
+import { BsChevronBarDown, BsFillPlayFill } from 'react-icons/bs';
 import FavoriteButton from './FavoriteButton';
+import useInfoModal from '@/hooks/useInfoModal';
 import { useRouter } from 'next/router';
 
 interface MovieCardProps {
@@ -9,12 +10,18 @@ interface MovieCardProps {
 
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
-
+    
     const router = useRouter()
+
+    const {openModal} = useInfoModal()
+
+    const handleOpenModal = useCallback(() => {
+        openModal(data?.id);
+    },[openModal,data?.id])
 
     return (
         <div className="group bg-zinc-900 col-span relative h-[12vw]">
-            <img src={data?.thumbnailUrl} alt="Movie" draggable={false} className="
+      <img src={data.thumbnailUrl} alt="Movie" draggable={false} className="
         cursor-pointer
         object-cover
         transition
@@ -27,7 +34,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
         w-full
         h-[12vw]
       " />
-            <div className="
+      <div className="
         opacity-0
         absolute
         top-0
@@ -44,7 +51,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
         group-hover:translate-x-[2vw]
         group-hover:opacity-100
       ">
-                <img src={data?.thumbnailUrl} alt="Movie" draggable={false} className="
+        <img src={data.thumbnailUrl} alt="Movie" draggable={false} className="
           cursor-pointer
           object-cover
           transition
@@ -54,7 +61,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
           w-full
           h-[12vw]
         " />
-                <div className="
+        <div className="
           z-10
           bg-zinc-800
           p-2
@@ -65,26 +72,27 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
           shadow-md
           rounded-b-md
           ">
-                    <div className="flex flex-row items-center gap-3">
-                        <div onClick={() => router.push(`/watch/${data?.id}`)} className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300">
-                            <BsFillPlayFill size={30} className="text-black w-4 lg:w-6" />
-                        </div>
-                        <div>
-                            <FavoriteButton movieId={data?.id}/>
-                        </div>
-                    </div>
-                    <p className='text-green-400 font-semibold mt-4'>
-                        New <span className='text-white'>2023</span>
-                    </p>
-                    <div className='flex flex-row mt-4 gap-2 items-center'>
-                        <p className='text-white text-[10px] lg:text-sm'>{data?.duration}</p>
-                    </div>
-                    <div className='flex flex-row mt-4 gap-2 items-center'>
-                        <p className='text-white text-[10px] lg:text-sm'>{data?.genre}</p>
-                    </div>
-                </div>
+          <div className="flex flex-row items-center gap-3">
+            <div  onClick={() => router.push(`/watch/${data?.id}`)} className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300">
+              <BsFillPlayFill className="text-black w-4 lg:w-6" />
             </div>
+            <FavoriteButton movieId={data.id} />
+            <div onClick={handleOpenModal} className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
+              <BsChevronBarDown className="text-white group-hover/item:text-neutral-300 w-4 lg:w-6" />
+            </div>
+          </div>
+          <p className="text-green-400 font-semibold mt-4">
+            New <span className="text-white">2023</span>
+          </p>
+          <div className="flex flex-row mt-4 gap-2 items-center"> 
+            <p className="text-white text-[10px] lg:text-sm">{data.duration}</p>
+          </div>
+          <div className="flex flex-row items-center gap-2 mt-4 text-[8px] text-white lg:text-sm">
+            <p>{data.genre}</p>
+          </div>
         </div>
+      </div>
+    </div>
     )
 }
 
